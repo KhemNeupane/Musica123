47378f4f0f6adcce90c5b5b2d6328b965727e7a4
665983 <neupaneanuj1@gmail.com>
2018-04-16 09:38:55 -0500

    dfdhfd8y

--- app/src/main/java/com/example/a646662/music/MainActivity.java
+++ app/src/main/java/com/example/a646662/music/MainActivity.java
@@ -17,21 +17,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_search:
-                    mTextMessage.setText(R.string.title_search);
-                    return true;
-                case R.id.navigation_play:
-                    mTextMessage.setText(R.string.title_download);
-                    return true;
+
+
                 case R.id.navigation_playlist:
-                    mTextMessage.setText(R.string.title_play);
-                    return true;
-                case R.id.navigation_history:
                     mTextMessage.setText(R.string.title_playlist);
                     return true;
-                case R.id.navigation_download:
-                    mTextMessage.setText(R.string.title_history);
-                    return true;
+
             }
             return true;
         }
